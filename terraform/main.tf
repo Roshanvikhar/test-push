6a210e44df8ac52a12d669925d209560357349e9
Roshanvikhar <roshanvikhar123@gmail.com>
2025-02-25 12:27:02 +0530

    adding

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"]
 }
 
 resource "aws_iam_role" "github_actions" {
